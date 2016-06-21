70ec22c8eed3db5c47d8ab905d5b5a67c69ec72a
Mike Sallese <msallese31@gmail.com>
2016-06-21 00:07:56 -0400

    begin run tests func

--- code/mikechap09.py
+++ code/mikechap09.py
@@ -1,16 +1,23 @@
 import thinkstats2
+import hypothesis
 import nsfg
 import first
 import thinkstats2
 import pandas
 
+def RunTests(sample):
+    n = len(sample)
+    print(n)
+    ht = hypothesis.DiffMeansPermute(sample)
+
 def main():
-    live, firsts, others = first.MakeFrames
-    n = 100000
+    live, firsts, others = first.MakeFrames()
+    n = len(live)
     for i in range(10):
         sample = thinkstats2.SampleRows(live, n)
-        Runtests(sample)
+        RunTests(sample)
         n = n // 2
 
-def RunTests:
-    
+if __name__ == '__main__':
+    main()
+
